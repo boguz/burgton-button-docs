73f383d488a36b662c59bc3bc49ddf0e1d776d50
Luis Pato <lpato@dfau.de>
2020-04-05 19:12:37 +0200

    [TASK] Add author section, smooth scroll to sections accounting for topbar height

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-const navElements = document.querySelectorAll('.nav__clickable');
+// Get Elements
+const navElements = document.querySelectorAll('.nav__link');
 
+// handle click on the menu items
 const handleNavClick = (event) => {
-  if (!event.target.classList.contains('nav__clickable--active')) {
-    document.querySelector('.nav__clickable--active').classList.remove('nav__clickable--active');
-    event.target.classList.add('nav__clickable--active');
+  event.preventDefault();
+
+  if (!event.target.classList.contains('nav__link--active')) {
+    toggleNavClass(event.target)
+    scrollToSection(event.target.getAttribute('href'))
   }
 }
 
+// Toggle the navigation active class
+const toggleNavClass = (target) => {
+  document.querySelector('.nav__link--active').classList.remove('nav__link--active');
+  target.classList.add('nav__link--active');
+}
+
+// Smooth scroll to the correct section,
+// accounting for the topbar height
+const scrollToSection = (sectionID) => {
+  const section = document.querySelector(sectionID);
+  const y = section.getBoundingClientRect().top + window.pageYOffset - 96;
+  window.scrollTo({top: y, behavior: 'smooth'});
+}
+
+// Event Listeners
 navElements.forEach((navElement) => navElement.addEventListener('click', handleNavClick));
